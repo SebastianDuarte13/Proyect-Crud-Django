5ec3c7ac4f655f1812f061fcf854da15795b0b77
SebastianDuarte13 <jsdr137@gmail.com>
2025-03-12 12:53:48 -0500

    :zap: advances crud productos :art:

--- crud/urls.py
+++ crud/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros/', views.nosotros, name='nosotros'),
+    path('productos/', views.productos, name='productos'),
+    path('productos/crear', views.crear, name='crear'),
+
 ]
--- crud/views.py
+++ crud/views.py
@@ -5,6 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Inicio</h1>")
+    return render(request, 'paginas/inicio.html')
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
+def productos(request):
+    return render(request, 'productos/indexp.html')
+def crear(request):
+    return render(request, 'productos/crear.html')
